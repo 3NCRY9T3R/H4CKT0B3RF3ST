61113bb1949d2ac80fd3fd66420e5ebb3deb72d5
JUGGERNAUT1209 <56041867+JUGGERNAUT1209@users.noreply.github.com>
2019-10-23 23:14:23 +0530

    Add files via upload

---
+++ code.cpp
@@ -0,0 +1,14 @@
+  
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int loop = 1;
+    while (loop <= 10){
+        cout << loop << endl ;
+        loop++;
+    }
+    return 0;
+}
