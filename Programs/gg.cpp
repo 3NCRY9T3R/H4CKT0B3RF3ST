842e080baaa64466ea6cb63f6c0b0d616fb105eb
JUGGERNAUT1209 <56041867+JUGGERNAUT1209@users.noreply.github.com>
2019-10-23 23:21:39 +0530

    Create gg.cpp

---
+++ Programs/gg.cpp
@@ -0,0 +1,14 @@
+  
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int loop = 1;
+    while (loop <= 100){
+        cout << loop << endl ;
+        loop++;
+    }
+    return 0;
+}
