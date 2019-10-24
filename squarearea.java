7abf41c490ff81ff416becdd4a48cc4e157a6dc5
gautam-arya <56232258+gautam-arya@users.noreply.github.com>
2019-10-24 05:45:31 +0530

    Create squarearea.java

---
+++ squarearea.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class SquareAreaDemo {
+   public static void main (String[] args)
+   {
+       System.out.println("Enter Side of Square:");
+       Scanner scanner = new Scanner(System.in);
+       double side = scanner.nextDouble();
+       double area = side*side; 
+       System.out.println("Area of Square is: "+area);
+   }
+}
