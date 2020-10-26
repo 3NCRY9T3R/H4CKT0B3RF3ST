57fbd3a930e517def64ae73dac1f5d6fc6457070
Ayush Mehrotra <ayushmehrotra1234@gmail.com>
2020-10-27 00:30:38 +0530

    Update calculator.py

--- Programs/Python/calculator.py
+++ Programs/Python/calculator.py
@@ -1,24 +1,40 @@
+def add(num1, num2): 
+    return num1 + num2 
 
-def add(x, y):
-    return x + y
+def subtract(num1, num2): 
+    return num1 - num2 
 
-def subtract(x, y):
-    return x - y
+def multiply(num1, num2): 
+    return num1 * num2 
 
+def divide(num1, num2): 
+    return num1 / num2
 
-def multiply(x, y):
-    return x * y
+print("Please select operation -\n" \ 
+		"1. Add\n" \ 
+		"2. Subtract\n" \ 
+		"3. Multiply\n" \ 
+		"4. Divide\n") 
 
+select = int(input("Select operations form 1, 2, 3, 4 :")) 
 
-def divide(x, y):
-    return x / y
+number_1 = int(input("Enter first number: ")) 
+number_2 = int(input("Enter second number: ")) 
 
+if select == 1: 
+	print(number_1, "+", number_2, "=", 
+					add(number_1, number_2)) 
 
-print("Select operation.")
-print("1.Add")
-print("2.Subtract")
-print("3.Multiply")
-print("4.Divide")
+elif select == 2: 
+	print(number_1, "-", number_2, "=", 
+					subtract(number_1, number_2)) 
 
-while True:
-    choice = input("Enter choice(1/2/3/4): ")
+elif select == 3: 
+	print(number_1, "*", number_2, "=", 
+					multiply(number_1, number_2)) 
+
+elif select == 4: 
+	print(number_1, "/", number_2, "=", 
+					divide(number_1, number_2)) 
+else: 
+	print("Invalid input") 
