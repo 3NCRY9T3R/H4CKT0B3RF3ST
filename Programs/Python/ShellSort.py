3f590b5a6841829215ad4777eb5ab6d0bcab7968
Ayush Mehrotra <ayushmehrotra1234@gmail.com>
2020-10-27 13:22:51 +0530

    Create ShellSort.py

---
+++ Programs/Python/ShellSort.py
@@ -0,0 +1,27 @@
+def shellSort(arr):  
+	n = len(arr) 
+	gap = n//2
+	while gap > 0: 
+
+		for i in range(gap,n): 
+			temp = arr[i] 
+			j = i 
+			while j >= gap and arr[j-gap] >temp: 
+				arr[j] = arr[j-gap] 
+				j -= gap 
+
+			arr[j] = temp 
+		gap //= 2
+    
+arr = [ 86,1,65,99,63,12] 
+
+n = len(arr) 
+print ("Array before sorting:") 
+for i in range(n): 
+	print(arr[i]), 
+
+shellSort(arr) 
+
+print ("\nArray after sorting:") 
+for i in range(n): 
+	print(arr[i])
